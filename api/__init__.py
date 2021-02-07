d29e0b7158183a20ceec0ba6e5ab057b4e10fe6a
moranAbramov <moran.abramov20@gmail.com>
2021-02-07 18:39:29 +0200

    generate secret key

--- api/__init__.py
+++ api/__init__.py
@@ -1,13 +1,13 @@
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
-
+import secrets
 
 # Construct the core application
 app = Flask(__name__)
 app.config.from_object(Config)
 app.config["DEBUG"] = True
-app.config["SECRET_KEY"] = "moran abramov"  # CHANGE THIS LINE
+app.config["SECRET_KEY"] = secrets.token_hex(16)
 db = SQLAlchemy(app)
 
 
