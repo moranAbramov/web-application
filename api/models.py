b078b16d870fa0b848a5c8fceb8013e49ec699b5
moranAbramov <moran.abramov20@gmail.com>
2021-02-07 17:52:47 +0200

    more changes

--- api/models.py
+++ api/models.py
@@ -1,12 +1,22 @@
 from api import db
+from sqlalchemy import inspect
 
 
 class People(db.Model):
+    id: str
+    title: str
+    content: str
+    viewes: int
+    timestamp: int
+
     id = db.Column(db.String(80), unique=True, nullable=False)
-    title = db.Column(db.String(80), unique=True, nullable=False)
-    content = db.Column(db.String(80), unique=True, nullable=False)
+    title = db.Column(db.String(80), nullable=False)
+    content = db.Column(db.String(80), nullable=False)
     views = db.Column(db.Integer, primary_key=True)
     timestamp = db.Column(db.Integer, primary_key=True)
+
+    def to_dict(self):
+        return {c.key: getattr(self, c.key) for c in inspect(self).mapper.column_attrs}
 
     def __repr__(self):
         return '<People %r>' % self.id
--- api/routes.py
+++ api/routes.py
@@ -1,49 +1,51 @@
-from flask import request, flash
+from flask import request, flash, jsonify
 from api import db, app
 from api.models import People
+from utils import apply_query
 
 
 # first page
 @app.route('/')
 def home_page():
-    new_people = People(id="ffffff",
-                        title="ffffff",
-                        content="ffffff",
-                        views=11511,
-                        timestamp=11511)  # Create an instance of the People class
-    db.session.add(new_people)  # Adds new People record to database
-    db.session.commit()  # Commits all changes
-    return "<h1>frgg</h1><p>SPARE</p>"
+    return "<h1>Welcome to the store!</h1>"
 
 
+# Takes query as input and returns matching entries
 @app.route('/store', methods=['GET'])
 def get_query():
     # Check if a query was provided as part of the URL.
     # If no query is provided, display an error in the browser.
     args = request.args
     if "query" in args:
-        ans = People.query.all()
-        return ans
-    return "No query string received"
+        store = People.query.all()
+        store_arr = []
+        apply_query(args["query"])
+        for people in store:
+            store_arr.append(people.to_dict())
+        return jsonify(store_arr)
+    else:
+        flash("No query was requested", 'error')
+        return "GET request done!"
 
 
+# Take entity and stores it
 @app.route('/store', methods=['POST'])
 def new_entity():
     # Create sql tables for our data models
     db.create_all()
     if not request.json['id'] or not request.json['title'] or not request.json['content'] or not request.json['views']\
             or not request.json['timestamp']:
-        flash('Please enter all the fields', 'error')  # missing information
+        flash('Please enter all the fields', 'error')  # missing fields
     else:
         new_people = People(id=request.json['id'],
                             title=request.json['title'],
                             content=request.json['content'],
                             views=request.json['views'],
                             timestamp=request.json['timestamp'])   # Create an instance of the People class
-        db.session.add(new_people)   # Adds new People record to database
+        db.session.add(new_people)   # Adds a new record to the database
         db.session.commit()  # Commits all changes
         flash('Record was successfully added')
-#    return "People adds successfully"
+    return "POST request done!"
 
 
 
